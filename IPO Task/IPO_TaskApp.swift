c7f264e666d18c4987a82e3390343f4216a373b8
Leo Lu <llu@lcs.on.ca>
2021-12-11 10:18:02 -0500

    Initial Commit

---
+++ IPO Task/IPO_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPO_TaskApp.swift
+//  IPO Task
+//
+//  Created by Leo Lu on 2021-12-11.
+//
+
+import SwiftUI
+
+@main
+struct IPO_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
